d90a0bf30cf63d02431531492d9a36a5ede05009
KYIV\Dmytro_Glomozda <Dmytro_Glomozda@epam.com>
2024-05-17 20:11:08 +0300

    - changed healthcheck message to "Health check passed - v2"

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def health_check():
-    return "Health check passed"
+    return "Health check passed - v2"
 
 if _name_ == "__main__":
     app.run(host="0.0.0.0", port=8080)
