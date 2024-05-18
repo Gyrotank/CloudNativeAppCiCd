d61658c54bbff4ac9c9419ed73aff9622cb764fc
KYIV\Dmytro_Glomozda <Dmytro_Glomozda@epam.com>
2024-05-18 20:02:11 +0300

    - updated app.py and Dockerfile

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def health_check():
     return "Health check passed - v2"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
